263cc05cce8cd254f6615716afb3788f2108d755
Ryan Goh Yu Ze <Ryan5yz@outlook.com>
2023-05-31 11:08:24 +0800

    Initial Commit

---
+++ quiz app/quiz_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_appApp.swift
+//  quiz app
+//
+//  Created by Ryan Goh on 27/5/23.
+//
+
+import SwiftUI
+
+@main
+struct quiz_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
